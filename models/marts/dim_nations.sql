e44e9ab74e76b1bb0d58092b1f1b248fd16cf467
mittallakshya2002 <lakshyamittal2002@gmail.com>
2025-04-04 07:33:50 +0000

    commit03

---
+++ models/marts/dim_nations.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='incremental')}}
+
+with nation as (
+    select
+            nation_id,
+            region_id,
+            name,
+            comment,
+            updated_ts 
+    from {{ ref('stg_nations')}}
+
+    {%if is_incremental() %}
+        where updated_ts > (select MAX(updated_ts) from {{this}} )
+    {%endif%}
+)
+
+select * from nation
---
+++ models/stage/descriptions.md
@@ -0,0 +1,2 @@
+{% docs example %} This is a test {% enddocs %}
+{% docs primary_key %} This is a primary key{% enddocs %}
